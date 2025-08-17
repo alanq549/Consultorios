36feb11451d4252e891d804a7e4702d2a6cf2d47
Alan Quintana <alanqff@gmail.com>
2025-08-17 13:30:58 -0600

    v1.4
    corrección de la ruta del generador de miniaturas..

--- client/src/pdfWorker.ts
+++ client/src/pdfWorker.ts
@@ -1,3 +1,3 @@
 import { GlobalWorkerOptions } from "pdfjs-dist";
 
-GlobalWorkerOptions.workerSrc = "/pdf.worker.min.js"; // apunta a public/
+GlobalWorkerOptions.workerSrc = import.meta.env.VITE_STATIC_URL + "/pdf.worker.min.js";
